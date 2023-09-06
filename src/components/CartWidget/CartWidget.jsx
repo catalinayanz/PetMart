3824a30aeffa8a76ebc339a2aa11864e471d3fb4
catalinayanzon <catalinayanzonok@gmail.com>
2023-09-06 14:39:35 -0300

    navbar

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,6 @@
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+
+const CartWidget = () => {
+    return(<> <AddShoppingCartOutlinedIcon/> <span>(7)</span> </>);
+}
+export default CartWidget;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import './Navbar.css';
+import CartWidget from '../CartWidget/CartWidget';
+
+const Navbar = () => {
+    return (<AppBar>
+        <Toolbar className='toolbar_navbar' sx={{display: "flex", justifyContent:"space-evenly"}}>
+            <Typography sx={{color: "black", fontSize: "25px"}}>
+                PetMart
+            </Typography>
+            <Button sx={{color: "white", fontSize:"18px"}}>
+                Inicio
+            </Button>
+            <Button sx={{color: "white", fontSize:"18px"}}>
+                Categorias
+            </Button>
+            <Button sx={{color: "white", fontSize:"18px"}}>
+                Productos
+            </Button>
+            <Button sx={{color: "white", fontSize:"18px"}}>
+                 Nosotros    
+            </Button>
+            <CartWidget />    
+        </Toolbar>
+    </AppBar>);
+}
+
+export default Navbar;
