923ac76953cae6d8eda66bb79a830e885a54915a
catalinayanzon <catalinayanzonok@gmail.com>
2023-09-26 15:30:38 -0300

    pasar todo a itemllistcontainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,53 +1,27 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { products } from "../../data/items";
-import { Item } from "../Item/Item";
-import { ItemList } from "../ItemList/ItemList";
-import { Loading } from "../Loading/Loading";
-import "./itemListContainer.css";
+import useAsyncMock from "../hooks/useAsyncMocks";
+import products from "../mocks/products.json";
+import { CircularProgress, Grid } from "@mui/material";
+import ProductDetail from "../products/ProductDetail";
+
+const CategoriesProductList = () => {
+    const {categoryId} = useParams();
+    const {data, loading} = useAsyncMock(products);
+
+    if(loading) return <CircularProgress />
+
+    const categorySelected = data.filter(category => category.category.toLowerCase() === categoryId.toLowerCase());
 
 
-export const ItemListContainer = () => {
 
-	const { idCategory } = useParams();
+    return( <div>
+        <Grid container spacing={3}>
+            {categorySelected.map((product)=>{
+                return <ProductDetail key={product.id} product={product}/>
+            })}
+             </Grid>
+    </div>
+    );
+}
 
-	const [loading, setLoading] = useState(true);
-
-	const [myProducts, setMyProducts] = useState();
-
-	useEffect(() => {
-		setLoading(true);
-		getItems(idCategory)
-			.then((response) => setMyProducts(response))
-			.finally(
-				setTimeout(() => {
-					setLoading(false);
-				}, 2000)
-			);
-	}, [idCategory]);
-
-	const getItems = (valueToFilter) => {
-		return new Promise((resolve) => {
-			if (valueToFilter === undefined) {
-				resolve(products);
-			} else {
-				resolve(
-					products.filter((item) => item.category === valueToFilter)
-				);
-			}
-		});
-	};
-
-	return loading ? (
-		<Loading />
-	) : (
-		<section className="container--ItemListContainer">
-			<h2 className="itemListContainer--title">
-				{idCategory === undefined ? "home" : idCategory}
-			</h2>
-			<div className="container--cards">
-				<ItemList myProducts={myProducts} />
-			</div>
-		</section>
-	);
-};
+export default CategoriesProductList;
